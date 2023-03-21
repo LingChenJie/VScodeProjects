7a9cf41667ad2ecf1a7cf10fff0b837ebc06813b
suqi <976918915@qq.com>
2023-03-21 18:34:08 +0800

    feat:commit code

--- cpp/mix/myfun.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "myfun.h"
-
-void display()
-{
-    printf("C++:http://c.biancheng/net/cplus/");
-}
--- cpp/mix/myfun.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void display();
-
-#ifdef __cplusplus
-}
-#endif;
